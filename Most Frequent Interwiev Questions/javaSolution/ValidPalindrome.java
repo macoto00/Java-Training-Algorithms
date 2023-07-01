49b0c18a758ad740c3d32d6f8b347e70d8549e83
macoto00 <117540231+macoto00@users.noreply.github.com>
2023-07-01 16:48:08 +0200

    Create ValidPalindrome.java

---
+++ Most Frequent Interwiev Questions/javaSolution/ValidPalindrome.java
@@ -0,0 +1,18 @@
+package javaSolution;
+/*
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters
+and removing all non-alphanumeric characters, it reads the same forward and backward.
+Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+ */
+public class ValidPalindrome {
+    public static void main(String[] args) {
+        String s = "A man, a plan, a canal: Panama";
+        System.out.println();
+    }
+
+    public static boolean isPalindrome(String s) {
+        
+    }
+}
