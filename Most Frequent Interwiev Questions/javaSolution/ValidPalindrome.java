470a1bef8aa895c536d9952384b141f375e1b438
macoto00 <117540231+macoto00@users.noreply.github.com>
2023-07-01 23:01:33 +0200

    Update ValidPalindrome.java

--- Most Frequent Interwiev Questions/javaSolution/ValidPalindrome.java
+++ Most Frequent Interwiev Questions/javaSolution/ValidPalindrome.java
@@ -9,10 +9,30 @@ Given a string s, return true if it is a palindrome, or false otherwise.
 public class ValidPalindrome {
     public static void main(String[] args) {
         String s = "A man, a plan, a canal: Panama";
-        System.out.println();
+        System.out.println(isPalindrome(s));
     }
 
     public static boolean isPalindrome(String s) {
-        
+        String clenaedString = s.toLowerCase().trim().replaceAll("[^a-zA-Z0-9]", "");
+
+        for (int i = 0; i < clenaedString.length() / 2; i++) {
+            if (clenaedString.charAt(i) != clenaedString.charAt(clenaedString.length() - 1 - i)) {
+                return false;
+            }
+        }
+
+        return true;
+
+        /*
+
+        or:
+
+        // Get the reversed cleaned string
+        String reversedCleanedString = new StringBuilder(cleanedString).reverse().toString();
+
+        // Check if the cleaned string is a palindrome
+        return cleanedString.equals(reversedCleanedString);
+
+         */
     }
 }
