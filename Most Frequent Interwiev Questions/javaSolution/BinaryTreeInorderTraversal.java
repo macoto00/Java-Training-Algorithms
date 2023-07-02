9f03bf023b64124f6ee450e4cb2d323c5b163d94
macoto00 <117540231+macoto00@users.noreply.github.com>
2023-07-02 19:10:37 +0200

    Create BinaryTreeInorderTraversal.java

---
+++ Most Frequent Interwiev Questions/javaSolution/BinaryTreeInorderTraversal.java
@@ -0,0 +1,9 @@
+package javaSolution;
+/*
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+ */
+public class BinaryTreeInorderTraversal {
+    public static void main(String[] args) {
+        
+    }
+}
