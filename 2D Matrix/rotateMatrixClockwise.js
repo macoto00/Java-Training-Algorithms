a896f482742451f54ae8cba017c04aab8edfaa71
TomasMacoun <macount@seznam.cz>
2023-08-22 10:31:11 +0200

    Create rotateMatrixClockwise.js

---
+++ 2D Matrix/rotateMatrixClockwise.js
@@ -0,0 +1,20 @@
+function rotateMatrixClockwise(matrix) {
+    // Your code here
+}
+
+const matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+
+rotateMatrixClockwise(matrix);
+
+console.log(matrix);
+
+// Output:
+// [
+//    [7, 4, 1],
+//    [8, 5, 2],
+//    [9, 6, 3]
+// ]
