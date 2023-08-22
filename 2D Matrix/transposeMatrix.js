47f2b96d992b11238c9fff75bfd4f5c2c27fe0cb
TomasMacoun <macount@seznam.cz>
2023-08-22 10:31:18 +0200

    Create transposeMatrix.js

---
+++ 2D Matrix/transposeMatrix.js
@@ -0,0 +1,35 @@
+// Input matrix
+const inputMatrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+
+function transposeMatrix(matrix) {
+    
+    let length = matrix.length;
+    let result = new Array(length);
+
+    for (let i = 0; i < length; i++) {
+        result[i] = new Array(length);
+    }
+
+    for (let i = 0; i < length; i++) {
+        for (let j = 0; j < length; j++) {
+            result[j][i] = matrix[i][j];
+        }
+    }
+
+    return result;
+}
+
+const transposedMatrix = transposeMatrix(inputMatrix);
+
+// Expected output (transposed matrix)
+// [
+//     [1, 4, 7],
+//     [2, 5, 8],
+//     [3, 6, 9]
+// ]
+
+console.log(transposedMatrix);
